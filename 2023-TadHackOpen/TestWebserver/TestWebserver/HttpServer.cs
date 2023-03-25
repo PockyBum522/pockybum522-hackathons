28665754173f0548acfa33760a7983fe58f60be6
DSikes <david@sikespc.com>
2023-03-25 13:49:13 -0400

    Added testwebserver

---
+++ 2023-TadHackOpen/TestWebserver/TestWebserver/HttpServer.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Text;
+
+namespace TestWebserver;
+
+static class HttpServer
+{
+    public static async Task HandleIncomingConnections()
+    {
+        bool runServer = true;
+
+        // While a user hasn't visited the `shutdown` url, keep on handling requests
+        while (runServer)
+        {
+            // Will wait here until we hear from a connection
+            HttpListenerContext ctx = await listener.GetContextAsync();
+
+            // Peel out the requests and response objects
+            HttpListenerRequest req = ctx.Request;
+            HttpListenerResponse resp = ctx.Response;
+
+            // Print out some info about the request
+            Console.WriteLine("Request #: {0}", ++requestCount);
+            Console.WriteLine(req.Url.ToString());
+            Console.WriteLine(req.HttpMethod);
+            Console.WriteLine(req.UserHostName);
+            Console.WriteLine(req.UserAgent);
+            Console.WriteLine();
+
+            // If `shutdown` url requested w/ POST, then shutdown the server after serving the page
+            if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/shutdown"))
+            {
+                Console.WriteLine("Shutdown requested");
+                runServer = false;
+            }
+
+            // Make sure we don't increment the page views counter if `favicon.ico` is requested
+            if (req.Url.AbsolutePath != "/favicon.ico")
+                pageViews += 1;
+
+            // Write the response info
+            string disableSubmit = !runServer ? "disabled" : "";
+            byte[] data = Encoding.UTF8.GetBytes(String.Format(pageData, pageViews, disableSubmit));
+            resp.ContentType = "text/html";
+            resp.ContentEncoding = Encoding.UTF8;
+            resp.ContentLength64 = data.LongLength;
+
+            // Write out to the response stream (asynchronously), then close it
+            await resp.OutputStream.WriteAsync(data, 0, data.Length);
+            resp.Close();
+        }
+    }
+}
---
+++ 2023-TadHackOpen/TestWebserver/TestWebserver/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TestWebserver;
+
+public static class Program
+{
+    public static HttpListener listener;
+    public static string url = "http://localhost:8000/";
+    public static int pageViews = 0;
+    public static int requestCount = 0;
+    public static string pageData = 
+        """<!DOCTYPE>
+                <html>
+                    <head>
+                        <title>HttpListener Example</title>
+                    </head>
+                    <body>
+                        <p>Page Views: {0}</p>
+                        <form method="post" action="shutdown">
+                        <input type="submit" value="Shutdown" {1}>
+                    </form>
+          </body>
+        </html>""";
+
+    public static void Main()
+    {
+        
+        // Create a Http server and start listening for incoming connections
+        var listener = new HttpListener();
+        listener.Prefixes.Add(url);
+        listener.Start();
+        Console.WriteLine("Listening for connections on {0}", url);
+
+        // Handle requests
+        Task listenTask = listener.HandleIncomingConnections();
+        listenTask.GetAwaiter().GetResult();
+
+        // Close the listener
+        listener.Close();
+    }
+}
