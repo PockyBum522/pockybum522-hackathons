24f23a8fa7f84b6c934cdc3d79596003067facc6
DSikes <david@sikespc.com>
2023-03-25 15:55:22 -0400

    Testwebserver works

--- 2023-TadHackOpen/TestWebserver/TestWebserver/HttpServer.cs
+++ 2023-TadHackOpen/TestWebserver/TestWebserver/HttpServer.cs
@@ -3,24 +3,54 @@ using System.Text;
 
 namespace TestWebserver;
 
-static class HttpServer
+public class HttpServer
 {
+    private static HttpListener? _listener = new();
+        
+    private static string url = "http://192.168.137.1:8000/";
+        
+    private static int _pageViews = 0;
+    private static int _requestCount = 0;
+        
+    private static string _pageData =>
+                            $"""
+                                <!DOCTYPE>
+                                        <html>
+                                            <head>
+                                                <title>HttpListener Example</title>
+                                            </head>
+                                            <body>
+                                                <p>Page Views: {_requestCount}</p>
+                                                
+                                            </body>
+                                </html>
+                            """;
+    //     <form method="post" action="shutdown">
+    // <input type="submit" value="Shutdown">
+    // </form>
     public static async Task HandleIncomingConnections()
     {
+       
         bool runServer = true;
 
+        _listener.Prefixes.Add(url);
+        _listener.Start();
+        
+        Console.WriteLine("Listening for connections on {0}", url);
+
+        
         // While a user hasn't visited the `shutdown` url, keep on handling requests
         while (runServer)
         {
             // Will wait here until we hear from a connection
-            HttpListenerContext ctx = await listener.GetContextAsync();
+            HttpListenerContext ctx = await _listener.GetContextAsync();
 
             // Peel out the requests and response objects
             HttpListenerRequest req = ctx.Request;
             HttpListenerResponse resp = ctx.Response;
 
             // Print out some info about the request
-            Console.WriteLine("Request #: {0}", ++requestCount);
+            Console.WriteLine("Request #: {0}", ++_requestCount);
             Console.WriteLine(req.Url.ToString());
             Console.WriteLine(req.HttpMethod);
             Console.WriteLine(req.UserHostName);
@@ -36,11 +66,11 @@ static class HttpServer
 
             // Make sure we don't increment the page views counter if `favicon.ico` is requested
             if (req.Url.AbsolutePath != "/favicon.ico")
-                pageViews += 1;
+                _pageViews += 1;
 
             // Write the response info
             string disableSubmit = !runServer ? "disabled" : "";
-            byte[] data = Encoding.UTF8.GetBytes(String.Format(pageData, pageViews, disableSubmit));
+            byte[] data = Encoding.UTF8.GetBytes(String.Format(_pageData, _pageViews, disableSubmit));
             resp.ContentType = "text/html";
             resp.ContentEncoding = Encoding.UTF8;
             resp.ContentLength64 = data.LongLength;
--- 2023-TadHackOpen/TestWebserver/TestWebserver/Program.cs
+++ 2023-TadHackOpen/TestWebserver/TestWebserver/Program.cs
@@ -8,38 +8,10 @@ namespace TestWebserver;
 
 public static class Program
 {
-    public static HttpListener listener;
-    public static string url = "http://localhost:8000/";
-    public static int pageViews = 0;
-    public static int requestCount = 0;
-    public static string pageData = 
-        """<!DOCTYPE>
-                <html>
-                    <head>
-                        <title>HttpListener Example</title>
-                    </head>
-                    <body>
-                        <p>Page Views: {0}</p>
-                        <form method="post" action="shutdown">
-                        <input type="submit" value="Shutdown" {1}>
-                    </form>
-          </body>
-        </html>""";
-
     public static void Main()
     {
-        
-        // Create a Http server and start listening for incoming connections
-        var listener = new HttpListener();
-        listener.Prefixes.Add(url);
-        listener.Start();
-        Console.WriteLine("Listening for connections on {0}", url);
-
         // Handle requests
-        Task listenTask = listener.HandleIncomingConnections();
+        var listenTask = HttpServer.HandleIncomingConnections();
         listenTask.GetAwaiter().GetResult();
-
-        // Close the listener
-        listener.Close();
     }
 }
