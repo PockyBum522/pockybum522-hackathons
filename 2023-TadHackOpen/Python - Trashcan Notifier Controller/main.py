3e457e434eefac914441cc1b0ab671c3955ea3c9
DSikes <david@sikespc.com>
2023-03-27 09:11:16 -0400

    Data from microcontroller to endpoint fixed
    Data from microcontroller is actually  getting to flask endpoint now

--- 2023-TadHackOpen/Python - Trashcan Notifier Controller/main.py
+++ 2023-TadHackOpen/Python - Trashcan Notifier Controller/main.py
@@ -1,11 +1,15 @@
 import os
+
 import werkzeug
 
 from flask import Flask
 from logic.flaskProductionServerWarningDisabler import flask_warning_suppressor
 
-if __name__ == "__main__":
+app = Flask(__name__)
 
+from endpoints import flaskEndpoints
+
+if __name__ == "__main__":
     print("")
     print(" * Starting trashcan notification controller...")
     print("")
@@ -14,7 +18,5 @@ if __name__ == "__main__":
     os.environ["PYTHONUNBUFFERED"] = "false"
 
     werkzeug.serving._ansi_style = flask_warning_suppressor(werkzeug.serving._ansi_style)
-
-    app = Flask(__name__)
 
     app.run(host='0.0.0.0', port=5000, debug=True)
