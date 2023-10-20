350dec25efa9fd2ef836a38d39f759f238713ac6
DSikes <david@sikespc.com>
2023-10-20 12:22:19 -0400

    Initialized project for simple HTTP server for jambonz

---
+++ 2023-TadHack/Code/Demos/JambonzTextToSpeechTest/JambonzTextToSpeechTest/HttpServer.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+
+namespace JambonzTextToSpeechTest;
+
+public class HttpServer
+{
+    public int Port = 8080;
+
+    private HttpListener _listener;
+
+    public void Start()
+    {
+        _listener = new HttpListener();
+        _listener.Prefixes.Add("http://*:" + Port.ToString() + "/");
+        _listener.Start();
+        Receive();
+    }
+
+    public void Stop()
+    {
+        _listener.Stop();
+    }
+
+    private void Receive()
+    {
+        _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+    }
+
+    private void ListenerCallback(IAsyncResult result)
+    {
+        if (_listener.IsListening)
+        {
+            var context = _listener.EndGetContext(result);
+            var request = context.Request;
+
+            // do something with the request
+            Console.WriteLine($"Request HTTP method: {request.HttpMethod} | Request URL: {request.Url}");
+
+            if (request.HasEntityBody)
+            {
+                var body = request.InputStream;
+                var encoding = request.ContentEncoding;
+                var reader = new StreamReader(body, encoding);
+                if (request.ContentType != null)
+                {
+                    Console.WriteLine("Client data content type {0}", request.ContentType);
+                }
+                Console.WriteLine("Client data content length {0}", request.ContentLength64);
+
+                Console.WriteLine("Start of data:");
+                string s = reader.ReadToEnd();
+                Console.WriteLine(s);
+                Console.WriteLine("End of data:");
+                reader.Close();
+                body.Close();
+            }
+
+            Receive();
+
+            var response = context.Response;
+            response.StatusCode = (int)HttpStatusCode.OK;
+            response.ContentType = "text/plain";
+            response.OutputStream.Write(new byte[] { }, 0, 0);
+            response.OutputStream.Close();
+        }
+    }
+}
--- 2023-TadHack/Code/Demos/JambonzTextToSpeechTest/JambonzTextToSpeechTest/Program.cs
+++ 2023-TadHack/Code/Demos/JambonzTextToSpeechTest/JambonzTextToSpeechTest/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace JambonzTextToSpeechTest;
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace JambonzTextToSpeechTest;
 
 internal static class Program
 {
-    public static void Main()
+    private static bool _keepRunning = true;
+
+    static async Task Main(string[] args)
     {
+        Console.CancelKeyPress += delegate(object? sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            Program._keepRunning = false;
+        };
+
+        Console.WriteLine("Starting HTTP listener...");
+
+        var httpServer = new HttpServer();
+        httpServer.Start();
+
+        while (Program._keepRunning) { }
+
+        httpServer.Stop();
+        await Task.Delay(1000); // Give it a sec to actually stop
         
+        Console.WriteLine("Exiting gracefully...");
     }
 }
