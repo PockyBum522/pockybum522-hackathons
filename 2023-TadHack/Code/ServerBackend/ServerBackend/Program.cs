f0ff3d6c8911a18ce00527a8338944a566e2fb4f
DSikes <david@sikespc.com>
2023-10-21 19:00:26 -0400

    Adding server backend which just currently indexes images to json file

---
+++ 2023-TadHack/Code/ServerBackend/ServerBackend/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ServerBackend;
+
+public static class Program
+{
+    private const string PathToStableDiffusionImages = @"C:\Users\jurrd\source\repos\pockybum522-hackathons\2023-TadHack\Code\Demos\TypescriptProjectDemo\resources\images";
+    
+    /// <summary>
+    /// Worker loop that does all backend tasks
+    /// </summary>
+    public static async Task Main()
+    {
+        while (true)
+        {
+            await WriteImagesJsonIndex();
+
+            await Task.Delay(10000);
+        }
+        
+        // ReSharper disable once FunctionNeverReturns because it's not supposed to
+    }
+
+    private static async Task WriteImagesJsonIndex()
+    {
+        var imagesIndex = Directory.GetFiles(PathToStableDiffusionImages);
+
+        var jsonString = "{\"";
+
+        for (var i = 0; i < imagesIndex.Length; i++)
+        {
+            var fullPath = imagesIndex[i];
+
+            jsonString += Path.GetFileName(fullPath);
+
+            if (i < imagesIndex.Length - 1)
+                jsonString += "\", \"";
+        }
+
+        jsonString += "\"}";
+
+        Console.WriteLine("Writing:");
+        Console.WriteLine(jsonString);
+        
+        var fullPathToJsonIndex = Path.Join(PathToStableDiffusionImages, "images_index.json");
+        
+        Console.WriteLine("To file:");
+        Console.WriteLine(fullPathToJsonIndex);
+        
+        await File.WriteAllTextAsync(fullPathToJsonIndex, jsonString);
+    }
+}
