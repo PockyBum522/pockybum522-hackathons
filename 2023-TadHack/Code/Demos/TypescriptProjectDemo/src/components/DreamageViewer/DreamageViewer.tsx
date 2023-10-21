0cdc18101c77f89aa74f0e34f3a1b9ac7f595bfe
Jared M Ashcraft <jaredmashcraft@gmail.com>
2023-10-21 17:25:17 -0400

    :construction: The bouncy bounce works!
    It goes bouncy bouncy bouncy bouncy bouncy bouncy

--- 2023-TadHack/Code/Demos/TypescriptProjectDemo/src/components/DreamageViewer/DreamageViewer.tsx
+++ 2023-TadHack/Code/Demos/TypescriptProjectDemo/src/components/DreamageViewer/DreamageViewer.tsx
@@ -1,24 +1,55 @@
-﻿import React, { useState, useEffect } from 'react';
+﻿import React, { useState, useEffect, useRef } from 'react';
 //import ReactDOM from 'react-dom/client'
 // 2048 x 1536
+// 1080/6
+// 768/6
+
+// figure out which dimension is longer
+// anchor by constraining dimension
+
 const DreamageViewer = () => {
 
-    const [xPos, setXPos] = useState(10);
-    const [yPos, setYPos] = useState(10);
+    // states
+    const [xPos, setXPos] = useState(0);
+    const [yPos, setYPos] = useState(0);
     const [xDir, setXDir] = useState(1);
-    const [yDir, setYDir] = useState(-1);
+    const [yDir, setYDir] = useState(1);
+    const [currWinWidth, setCurrWinWidth] = useState(window.innerWidth);
+    const [currWinHeight, setCurrWinHeight] = useState(window.innerHeight);
     
+    // refs
+    const mainImgEl = useRef<HTMLImageElement>(null);
     
     useEffect(() => {
+        
         const timer = setTimeout(() => {
-            setXPos(xPos+xDir);
-            setYPos(yPos+yDir);
-        }, 15)
+            if (mainImgEl.current !== null) {
+                if (yPos + mainImgEl.current.getBoundingClientRect().height >= currWinHeight)
+                    setYDir(-1);
+                if (yPos < 0)
+                    setYDir(1);
+                if (xPos + mainImgEl.current.getBoundingClientRect().width >= currWinWidth)
+                    setXDir(-1);
+                if (xPos < 0)
+                    setXDir(1);
+            }
+            
+            setYPos(yPos + yDir);
+            setXPos(xPos + xDir);
+        }, 1)
     }, [xPos, yPos]);
+    
+    useEffect(() => {
+        
+    }, [currWinWidth]);
+    
+    useEffect(() => {
+        
+    }, [currWinHeight]);
     
     return (
         <div className={"relative"}>
-            <img style={{"top": yPos, "left": xPos}} className={`absolute rounded-sm`} src={"https://placehold.co/600x400"} alt={"placeholder"}></img>
+            <img id="main_img" ref={mainImgEl} style={{"top": yPos, "left": xPos, "width": Math.floor(window.innerWidth / 2)}} className={`absolute rounded-sm`} src={`https://placehold.co/2048x1536`} alt={"placeholder"}></img>
         </div>
     )
 }
