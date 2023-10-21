a976a77d609fd82584d5c2dd32a27a46f78e8eac
DSikes <david@sikespc.com>
2023-10-21 12:57:13 -0400

    Added some more test images

--- 2023-TadHack/Code/Demos/DezgoStableDiffusionTest/DezgoStableDiffusionTest/Program.cs
+++ 2023-TadHack/Code/Demos/DezgoStableDiffusionTest/DezgoStableDiffusionTest/Program.cs
@@ -12,22 +12,22 @@ class Program
         // const string llmUserPrompt = "well by golly gee whiz it was fuckin evanescence, i have listened to them my whole life ever since my middle school graduation";
 
         // What is your favorite comfort food and why?
-        const string llmUserPrompt = "I love macaroni and cheese. It is so rich and delicious and I think it is my favorite thing that is yellow";
+        // const string llmUserPrompt = "I love macaroni and cheese. It is so rich and delicious and I think it is my favorite thing that is yellow";
         
         // What is your favorite beverage and why?
         // const string llmUserPrompt = "I love coffee! Coffee is so delicious. It is my favorite thing to drink on cold winter days";
 
         // Describe your favorite hobby
-        // const string llmUserPrompt = "I like to sharpen knives and then they are fun to use to cook with when they are nice and sharp";
+        //const string llmUserPrompt = "I like to sharpen knives and then they are fun to use to cook with when they are nice and sharp";
 
         // Describe your favorite place on earth
-        // const string llmUserPrompt = "My favorite place on earth is North Carolina, especially when the leaves are turning pretty colors in fall. I love anywhere that has lots of trees and rivers";
+        //const string llmUserPrompt = "My favorite place on earth is North Carolina, especially when the leaves are turning pretty colors in fall. I love anywhere that has lots of trees and rivers";
 
         // Describe your dream vacation
         // const string llmUserPrompt = "My dream vacation would be to Japan because I want to see all the hentai";
 
         // Describe your favorite animal
-        // const string llmUserPrompt = "My favorite animal is the gray wolf, I like that they live in packs and help each other out";
+        const string llmUserPrompt = "My favorite animal is the gray wolf, I like that they live in packs and help each other out";
 
         // Describe your favorite book
         // const string llmUserPrompt = "My favorite book is the Bible because Jesus was a cool dude who kills aliens and doesn't afraid of anything";
