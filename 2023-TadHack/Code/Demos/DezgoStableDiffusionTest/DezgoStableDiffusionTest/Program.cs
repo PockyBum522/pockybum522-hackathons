6d6c4a513230b2866caf5f9925dd0c3d76c4a54c
DSikes <david@sikespc.com>
2023-10-21 12:48:48 -0400

    Updated LLM prompt to start testing images off test responses

--- 2023-TadHack/Code/Demos/DezgoStableDiffusionTest/DezgoStableDiffusionTest/Program.cs
+++ 2023-TadHack/Code/Demos/DezgoStableDiffusionTest/DezgoStableDiffusionTest/Program.cs
@@ -6,9 +6,43 @@ class Program
 {
     public static async Task Main()
     {
-        var llmUserPrompt = "well by golly gee whiz it was fuckin evanescence, i have listened to them my whole life ever since my middle school graduation";
+        // User response examples:
+        
+        // What was your favorite song or artist when you were 17 years old and why?
+        // const string llmUserPrompt = "well by golly gee whiz it was fuckin evanescence, i have listened to them my whole life ever since my middle school graduation";
 
-        var llmPromptPrefix = "A creative interpretation of the following, in a random art style: ";
+        // What is your favorite comfort food and why?
+        const string llmUserPrompt = "I love macaroni and cheese. It is so rich and delicious and I think it is my favorite thing that is yellow";
+        
+        // What is your favorite beverage and why?
+        // const string llmUserPrompt = "I love coffee! Coffee is so delicious. It is my favorite thing to drink on cold winter days";
+
+        // Describe your favorite hobby
+        // const string llmUserPrompt = "I like to sharpen knives and then they are fun to use to cook with when they are nice and sharp";
+
+        // Describe your favorite place on earth
+        // const string llmUserPrompt = "My favorite place on earth is North Carolina, especially when the leaves are turning pretty colors in fall. I love anywhere that has lots of trees and rivers";
+
+        // Describe your dream vacation
+        // const string llmUserPrompt = "My dream vacation would be to Japan because I want to see all the hentai";
+
+        // Describe your favorite animal
+        // const string llmUserPrompt = "My favorite animal is the gray wolf, I like that they live in packs and help each other out";
+
+        // Describe your favorite book
+        // const string llmUserPrompt = "My favorite book is the Bible because Jesus was a cool dude who kills aliens and doesn't afraid of anything";
+
+        // Describe your favorite halloween costume
+        // const string llmUserPrompt = "The best Halloween costume is a Jedi night, especially if you have a lightsaber and everyone knows the best color. Lightsaber is purple because Samuel l. Jackson actually requested that his lightsaber be purple";
+
+        // What is your favorite color and why
+        // const string llmUserPrompt = "My favorite color is purple because that is the color of mace windows lightsaber and he is the coolest Jedi";
+
+        // What was your favorite toy as a child
+        // const string llmUserPrompt = "My favorite toy is a child was that carpet that has like a town on it and it has cars and streets and roads and rivers and other things and I think there's people on it and then there's probably some other things and this is kind of just rambling and I just want to see what happens";
+
+        // Prefix for the LLM, will be prepended to the prompt
+        const string llmPromptPrefix = "A creative interpretation of the following, in a random art style: ";
         
         var client = new HttpClient();
 
