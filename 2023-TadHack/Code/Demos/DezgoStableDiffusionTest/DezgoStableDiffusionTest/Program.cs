ee28e4912c26bd9573a4d603dd29d111496ad50a
DSikes <david@sikespc.com>
2023-10-22 09:22:32 -0400

    Auto generating images with backend now working

--- 2023-TadHack/Code/Demos/DezgoStableDiffusionTest/DezgoStableDiffusionTest/Program.cs
+++ 2023-TadHack/Code/Demos/DezgoStableDiffusionTest/DezgoStableDiffusionTest/Program.cs
@@ -39,72 +39,8 @@ class Program
         // const string llmUserPrompt = "My favorite color is purple because that is the color of mace windows lightsaber and he is the coolest Jedi";
 
         // What was your favorite toy as a child
-        // const string llmUserPrompt = "My favorite toy is a child was that carpet that has like a town on it and it has cars and streets and roads and rivers and other things and I think there's people on it and then there's probably some other things and this is kind of just rambling and I just want to see what happens";
 
-        // Prefix for the LLM, will be prepended to the prompt
-        const string llmPromptPrefix = "A creative interpretation of the following, in a random art style: ";
-        
-        var client = new HttpClient();
-
-        var request = new HttpRequestMessage
-        {
-            Method = HttpMethod.Post,
-            RequestUri = new Uri("https://api.dezgo.com/text2image"),
-            Headers =
-            {
-                { "X-Dezgo-Key", SECRETS.DezgoApiKey },
-                // { "Content-Type", "application/json" },
-            },
-            Content = new FormUrlEncodedContent(new Dictionary<string, string>
-            {
-                { "width", "1024" },
-                { "height", "768" },
-                { "model", "epic_diffusion_1_1" },
-                { "negative_prompt", "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, blurry, bad anatomy, blurred, watermark, grainy, signature, cut off, draft" },
-                { "sampler", "dpmpp_2m_karras" },
-                { "steps", "30" },
-                { "guidance", "10" },
-                { "prompt", llmPromptPrefix + llmUserPrompt },
-                { "upscale", "2" },
-                
-            }),
-        };
-
-        using var response = await client.SendAsync(request);
-
-        response.EnsureSuccessStatusCode();
-
-        var responseHeaders = response.Headers;
-
-        foreach (var rHeader in responseHeaders)
-        {
-            Console.WriteLine($"Header key: {rHeader.Key}");
-
-            var valueCounter = 1;
-            foreach (var hValue in rHeader.Value)
-            {
-                Console.WriteLine($"Header value {valueCounter++}: {hValue}");
-            }
-        }
-
-        Console.WriteLine();
-
-
-        var body = await response.Content.ReadAsStreamAsync();
-
-        var filesafeTimeStamp = DateTimeOffset.Now.ToString("T").Replace(":", "-");
-        
-        var outFilePath = @$"D:\Dropbox\Documents\Desktop\test_{filesafeTimeStamp}.png"; 
-        
-        File.Delete(outFilePath);
-        
-        SaveStreamAsFile(outFilePath, body);
     }
 
-    private static void SaveStreamAsFile(string fullFilePath, Stream inputStream) 
-    {  
-        using var outputFileStream = new FileStream(fullFilePath, FileMode.Create);
-        
-        inputStream.CopyTo(outputFileStream);
-    } 
+
 }
--- 2023-TadHack/Code/ServerBackend/ServerBackend/Program.cs
+++ 2023-TadHack/Code/ServerBackend/ServerBackend/Program.cs
@@ -7,6 +7,7 @@ namespace ServerBackend;
 
 public static class Program
 {
+    private const string KeysValuesCachePath = @"C:\Users\Public\Documents\cached_key_value_pairs.txt"; 
     private const string PathToStableDiffusionImages = @"C:\Users\jurrd\source\repos\pockybum522-hackathons\2023-TadHack\Code\Demos\TypescriptProjectDemo\resources\images";
     
     /// <summary>
@@ -22,19 +23,122 @@ public static class Program
             
             foreach (var rawValue in stacuityKeysValuesList)
             {
-                Console.Write("Key: ");
-                Console.WriteLine(
-                    ParseStacuityKey(rawValue));
+                var keyValueStringSeenAlready = KeyValuePairSeenAlready(rawValue);
                 
-                Console.Write("Value: ");
-                Console.WriteLine(
+                if (keyValueStringSeenAlready) continue;
+
+                await SaveNewDezgoGeneratedImage(
+                    ParseStacuityKey(rawValue),
                     ParseStacuityValue(rawValue));
+
+                CacheRawKeyValueStringAsSeen(rawValue);
             }
             
             await Task.Delay(10000);
         }
         
         // ReSharper disable once FunctionNeverReturns because it's not supposed to
+    }
+
+    private static async Task SaveNewDezgoGeneratedImage(string promptUserSaw, string imagePrompt)
+    {
+        // Prefix for the LLM, will be prepended to the prompt
+        const string llmPromptPrefix = "A creative interpretation of the following, in a random art style: ";
+        
+        var client = new HttpClient();
+
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("https://api.dezgo.com/text2image"),
+            Headers =
+            {
+                { "X-Dezgo-Key", SECRETS.DezgoApiKey },
+            },
+            Content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "width", "1024" },
+                { "height", "768" },
+                { "model", "epic_diffusion_1_1" },
+                { "negative_prompt", "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, blurry, bad anatomy, blurred, watermark, grainy, signature, cut off, draft" },
+                { "sampler", "dpmpp_2m_karras" },
+                { "steps", "30" },
+                { "guidance", "10" },
+                { "prompt", llmPromptPrefix + imagePrompt },
+                { "upscale", "2" },
+                
+            }),
+        };
+
+        using var response = await client.SendAsync(request);
+
+        response.EnsureSuccessStatusCode();
+
+        var responseHeaders = response.Headers;
+
+        foreach (var rHeader in responseHeaders)
+        {
+            Console.WriteLine($"Header key: {rHeader.Key}");
+
+            var valueCounter = 1;
+            foreach (var hValue in rHeader.Value)
+            {
+                Console.WriteLine($"Header value {valueCounter++}: {hValue}");
+            }
+        }
+
+        Console.WriteLine();
+
+        var body = await response.Content.ReadAsStreamAsync();
+
+        
+        var filesafeTimeStamp = DateTimeOffset.Now.ToString("d").Replace("/", "-");
+        filesafeTimeStamp += '_';
+        filesafeTimeStamp += DateTimeOffset.Now.ToString("T").Replace(":", "-");
+
+        var filesafeUserPrompt = promptUserSaw.Replace("?", "");
+        filesafeUserPrompt = filesafeUserPrompt.Replace(".", "");
+        filesafeUserPrompt = filesafeUserPrompt.Replace(" ", "_");
+        filesafeUserPrompt = filesafeUserPrompt.ToLower();
+        
+        var outFilePath = @$"D:\Dropbox\Documents\Desktop\{filesafeUserPrompt}_{filesafeTimeStamp}.png"; 
+        
+        File.Delete(outFilePath);
+        
+        SaveStreamAsFile(outFilePath, body);
+    }
+
+    private static void SaveStreamAsFile(string fullFilePath, Stream inputStream) 
+    {  
+        using var outputFileStream = new FileStream(fullFilePath, FileMode.Create);
+        
+        inputStream.CopyTo(outputFileStream);
+    } 
+    
+    private static void CacheRawKeyValueStringAsSeen(string rawValue)
+    {
+        if (!File.Exists(KeysValuesCachePath))
+            File.Create(KeysValuesCachePath).Close();
+
+        File.AppendAllText(KeysValuesCachePath, Environment.NewLine + rawValue);
+    }
+
+    private static bool KeyValuePairSeenAlready(string rawValueToCheck)
+    {
+        if (!File.Exists(KeysValuesCachePath))
+            File.Create(KeysValuesCachePath).Close();
+        
+        var cachedKeysValues = File.ReadAllLines(KeysValuesCachePath);
+
+        var isKeyValueInCache = false;
+        
+        foreach (var line in cachedKeysValues)
+        {
+            if (line == rawValueToCheck)
+                isKeyValueInCache = true;
+        }
+
+        return isKeyValueInCache;
     }
 
     private static string ParseStacuityKey(string rawValue)
