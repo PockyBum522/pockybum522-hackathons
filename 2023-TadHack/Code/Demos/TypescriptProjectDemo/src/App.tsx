4b25055628932c9e37a33c7ea0afae172e96be6f
Jared M Ashcraft <jaredmashcraft@gmail.com>
2023-10-22 10:40:30 -0400

    Most recent DreamageViewer update

--- 2023-TadHack/Code/Demos/TypescriptProjectDemo/src/App.tsx
+++ 2023-TadHack/Code/Demos/TypescriptProjectDemo/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App overflow-hidden">
+    <div className="App">
       <DreamageViewer />
     </div>
   );
--- 2023-TadHack/Code/Demos/TypescriptProjectDemo/src/components/DreamageViewer/DreamageViewer.tsx
+++ 2023-TadHack/Code/Demos/TypescriptProjectDemo/src/components/DreamageViewer/DreamageViewer.tsx
@@ -1,4 +1,5 @@
 ﻿import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 // 2048 x 1536
 // 1080/6
 // 768/6
@@ -8,21 +9,87 @@
 
 const DreamageViewer = () => {
 
+    // local consts
+    const refreshRateImgMovement = 10; // in milliseconds
+    const refreshRateImgCarousel = 2000; // in milliseconds
+    
     // states
     const [xPos, setXPos] = useState(0);
     const [yPos, setYPos] = useState(0);
     const [xDir, setXDir] = useState(1);
     const [yDir, setYDir] = useState(1);
+    const [screenImageCount, setScreenImageCount] = useState(6);
+    const [posArray, setPosArray] = useState<Object[]>([]);
+    const [currImgIndex, setCurrImgIndex] = useState(0);
     const [currWinWidth, setCurrWinWidth] = useState(window.innerWidth);
     const [currWinHeight, setCurrWinHeight] = useState(window.innerHeight);
-    const [imagesArray, setImagesArray] = useState<Object>([]);
+    const [imagePathArray, setimagePathArray] = useState<string[]>([]);
+    const [imageTagsArray, setImageTagsArray] = useState<React.ReactElement[]>([]); 
     
     // refs
     const mainImgEl = useRef<HTMLImageElement>(null);
     
     useEffect(() => {
         
-        const timer = setTimeout(() => {
+       axios.get("http://pockybum522.com:8080/images/images_index.json")
+           
+           .then((response) => {
+               
+               const filenames = response.data.images_filenames;
+               let filenamesArray:string[] = [];
+               let randomPosArray:Object[] = [];
+               
+               //console.log(filenames)
+               
+               for (let el in filenames) {
+                   filenamesArray.push(filenames[el]);
+                   
+               }
+               
+               // need to filter the array to only allow for the png image files and nothing else
+               const filenamesArrayFiltered = filenamesArray.filter((path) => /\.(jpe?g|png|webp)$/.test(path));
+               
+               for (let filename in filenamesArrayFiltered) {
+                   if (mainImgEl.current !== null) {
+                       const randomXPos = Math.floor(Math.abs(Math.random() * currWinWidth - mainImgEl.current.getBoundingClientRect().right));
+                       const randomYPos = Math.floor(Math.abs(Math.random() * currWinHeight - mainImgEl.current.getBoundingClientRect().right));
+                       const randomPos = {xPos: randomXPos, yPos: randomYPos};
+                       randomPosArray.push(randomPos);
+                   }
+               }
+               
+               setimagePathArray(filenamesArrayFiltered);
+               setPosArray(randomPosArray);
+               
+           });
+    }, []);
+    
+    useEffect(() => {
+        
+        const carouselTimer = setTimeout(() => {
+            
+            console.log(`${currImgIndex} > ${imagePathArray.length}`);
+            
+            if (currImgIndex === imagePathArray.length - 1)
+                setCurrImgIndex(0);
+            else 
+                setCurrImgIndex(currImgIndex + 1);
+            
+        }, refreshRateImgCarousel)
+        
+    }, [currImgIndex]);
+    
+    useEffect(() => {
+        
+        console.log(imagePathArray);
+        
+    }, [imagePathArray]);
+    
+    useEffect(() => {
+        
+        const imgMovementTimer = setTimeout(() => {
+            
+            
             if (mainImgEl.current !== null) {
                 if (yPos + mainImgEl.current.getBoundingClientRect().height >= currWinHeight)
                     setYDir(-1);
@@ -36,7 +103,9 @@ const DreamageViewer = () => {
             
             setYPos(yPos + yDir);
             setXPos(xPos + xDir);
-        }, 1)
+            
+        }, refreshRateImgMovement)
+        
     }, [xPos, yPos]);
     
     useEffect(() => {
@@ -46,10 +115,15 @@ const DreamageViewer = () => {
     useEffect(() => {
         
     }, [currWinHeight]);
+
+    const PROTOCOL = "http";
+    //const DOMAIN = "pockybum522.com";
+    const DOMAIN = "100.110.42.90";
+    const PORT = "8080";
     
     return (
         <div className={"relative"}>
-            <img id="main_img" ref={mainImgEl} style={{"top": yPos, "left": xPos, "width": Math.floor(window.innerWidth / 2)}} className={`absolute rounded-sm`} src={`https://placehold.co/2048x1536`} alt={"placeholder"}></img>
+            <img id="main_img" ref={mainImgEl} style={{"top": yPos, "left": xPos, "width": Math.floor(window.innerWidth / 2)}} className={`absolute rounded-lg`} src={`${PROTOCOL}://${DOMAIN}:${PORT}/images/${imagePathArray[currImgIndex]}`} alt={"placeholder"}></img>
         </div>
     )
 }
