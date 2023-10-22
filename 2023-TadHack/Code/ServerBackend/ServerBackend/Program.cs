39b569a3c8cd5119908eb0dede6c3ff9df8ba052
DSikes <david@sikespc.com>
2023-10-22 08:36:30 -0400

    Stacuity public API key retrieval working

--- 2023-TadHack/Code/ServerBackend/ServerBackend/Program.cs
+++ 2023-TadHack/Code/ServerBackend/ServerBackend/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ServerBackend;
+﻿using DezgoStableDiffusionTest;
+
+namespace ServerBackend;
 
 public static class Program
 {
@@ -13,6 +15,8 @@ public static class Program
         {
             await WriteImagesJsonIndex();
 
+            await CheckForNewStacuityKeys();
+
             await Task.Delay(10000);
         }
         
@@ -21,6 +25,12 @@ public static class Program
 
     private static async Task WriteImagesJsonIndex()
     {
+        if (!Directory.Exists(PathToStableDiffusionImages))
+        {
+            Console.WriteLine($"Could not find anything at path: {PathToStableDiffusionImages}");
+            return;
+        }
+        
         var imagesIndex = Directory.GetFiles(PathToStableDiffusionImages);
 
         var jsonString = "{ \"images_filenames\" : [ \"";
@@ -47,4 +57,43 @@ public static class Program
         
         await File.WriteAllTextAsync(fullPathToJsonIndex, jsonString);
     }
+
+    private static async Task CheckForNewStacuityKeys()
+    {
+        var client = new HttpClient();
+
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("https://api.stacuity.com/api/v1/endpoints/b257522a-4d83-4f3e-8954-71d296bbf7c1/keyvalues"),
+            Headers =
+            {
+                { "Authorization", $"Bearer {SECRETS.StacuityApiBearerToken}" },
+            },
+        };
+
+        using var response = await client.SendAsync(request);
+
+        response.EnsureSuccessStatusCode();
+
+        var responseHeaders = response.Headers;
+
+        foreach (var rHeader in responseHeaders)
+        {
+            Console.WriteLine($"Header key: {rHeader.Key}");
+
+            var valueCounter = 1;
+            foreach (var hValue in rHeader.Value)
+            {
+                Console.WriteLine($"Header value {valueCounter++}: {hValue}");
+            }
+        }
+
+        Console.WriteLine();
+
+        var body = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine("Response:");
+        Console.WriteLine(body);
+    }    
 }
