08684b904f391085f4ba3eaa5da7639094f95d5c
DSikes <david@sikespc.com>
2023-12-01 10:25:53 -0500

    day 07a initial commit

---
+++ 2022-AoC-CSharp/Day_07a/AoC 2022 CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+
+namespace AoC_2022_CSharp;
+
+internal static class Program
+{
+    public static ILogger Logger = LoggerSetup.BuildLogger(); 
+    
+    public static void Main()
+    {
+        var rawString = RawData.ActualData;
+
+        
+        
+        Logger.Debug("First position after X number of unique chars: {FinalAnswer}");
+    }
+}
---
+++ 2022-AoC-CSharp/Day_07a/AoC 2022 CSharp/RawData.cs
@@ -0,0 +1,12 @@
+﻿namespace AoC_2022_CSharp;
+
+public class RawData
+{
+    public static string SampleData01 => """
+
+                                         """;
+
+    public static string ActualData => """
+
+                                       """;
+}
