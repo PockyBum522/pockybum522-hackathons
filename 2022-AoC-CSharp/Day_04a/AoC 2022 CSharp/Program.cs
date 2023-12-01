c9b9d5ce5e3f18f11d125dfc3fcb62531e0f9da2
DSikes <david@sikespc.com>
2023-12-01 05:07:35 -0500

    2022 day 04 initial commit
    - 2022 day 04 initial commit
    - Also copied as template to get ready for 2023 day 02a

---
+++ 2022-AoC-CSharp/Day_04a/AoC 2022 CSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+
+namespace AoC_2022_CSharp;
+
+internal static class Program
+{
+    public static ILogger Logger = LoggerSetup.BuildLogger(); 
+    
+    public static void Main()
+    {
+        var dataLines = RawData.SampleDataPartA
+            .Split(Environment.NewLine);
+
+        var total = 0;
+
+        for (var i = 0; i < dataLines.Length; i++)
+        {
+            
+            
+            // total += 
+            
+            // Logger.Debug("Group lines: {RawLine}", string.Join(" | ", groupLines));
+        }
+
+        Logger.Information("Total: {Total}", total);
+    }
+}
---
+++ 2023-AoC-CSharp/Day_02a/AoC 2022 CSharp/RawData.cs
@@ -0,0 +1,16 @@
+﻿namespace AoC_2022_CSharp;
+
+public class RawData
+{
+    public static string SampleDataPartA => """
+                                            
+                                            """;
+
+    // public static string SampleDataPartB => """
+    //                                         
+    //                                         """;
+
+    public static string ActualData => """
+                                       
+                                       """;
+}
