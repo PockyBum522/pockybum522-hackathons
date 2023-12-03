9338f55bc24e426c4ab5b800b9538d307c7886e0
DSikes <david@sikespc.com>
2023-12-03 09:05:10 -0500

    2023 day 04a initial

---
+++ 2023-AoC-CSharp/Day_04a/AoC 2023 CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+namespace AoC_2023_CSharp;
+
+internal static class Program
+{
+    private static readonly ILogger Logger = LoggerSetup.BuildLogger(); 
+    
+    public static void Main()
+    {
+        var rawLines = RawData.ActualData
+            .Split(Environment.NewLine);
+
+        var totalCumulative = 0;
+        
+        foreach (var line in rawLines)
+        {
+            
+            
+            totalCumulative += 1;
+        }
+        
+        Logger.Information("Answer: {Total}", totalCumulative);
+    }
+}
