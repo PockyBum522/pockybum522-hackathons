d6c61537b7b6b74b893c92c0309a1f4b2d0d1d13
DSikes <david@sikespc.com>
2023-12-06 10:06:49 -0500

    2023 Day 06a initial commmit

--- 2023-AoC-CSharp/Day_06a/AoC 2023 CSharp/Program.cs
+++ 2023-AoC-CSharp/Day_06a/AoC 2023 CSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Serilog;
+﻿using AoC_2023_CSharp.Models;
+using Serilog;
 using Serilog.Events;
 
 namespace AoC_2023_CSharp;
@@ -6,25 +7,81 @@ namespace AoC_2023_CSharp;
 internal static class Program
 {
     private static readonly ILogger Logger = LoggerSetup.ConfigureLogger()
-            .MinimumLevel.Information()
-            .CreateLogger(); 
-    
+            .MinimumLevel.Verbose()
+            .CreateLogger();
+
+    private static List<int> _records;
+
     public static void Main()
     {
         Logger.Information("Starting!");
         
-        var rawLines = RawData.ActualData
+        var rawLines = RawData.SampleData01
             .Split(Environment.NewLine);
 
-        var answerTotal = 0;
+        List<int> times = new List<int>();
+        List<int> distances = new List<int>();
         
         foreach (var line in rawLines)
         {
+            if (line.StartsWith("Time: "))
+            {
+                var timesStrings = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (var i = 1; i < timesStrings.Length; i++)
+                {
+                    times.Add(int.Parse(timesStrings[i]));
+                }
+                
+                Logger.Debug("{@TimesStrings}", timesStrings);
+            }
             
-            
-            //answerTotal += 1;
+            if (line.StartsWith("Distance: "))
+            {
+                var distancesStrings = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                
+                for (var i = 1; i < distancesStrings.Length; i++)
+                {
+                    distances.Add(int.Parse(distancesStrings[i]));
+                }
+                
+                Logger.Debug("{@DistStrings}", distancesStrings);
+            }
+        }
+
+        _records = new List<int>();
+
+        for (var i = 0; i < times.Count; i++)
+        {
+            RunAllPossibleButtonTimes(i, times, distances);
+        }
+
+        var answer = 0;
+        
+        foreach (var record in _records)
+        {
+            answer *= record;
         }
         
-        Logger.Information("Answer: {AnswerTotal}", answerTotal);
+        Logger.Debug("Records you got: {@Records}", _records);
+        Logger.Information("Final answer: {Answer}", answer);
+    }
+
+    private static void RunAllPossibleButtonTimes(int whichRace, List<int> times, List<int> distances)
+    {
+        for (var i = 0; i < times[whichRace] - 1; i++)
+        {
+            var buttonPressedTime = i;
+
+            var thisBoat = new Boat(Logger, buttonPressedTime);
+
+            thisBoat.RunRace(times[whichRace]);
+
+            if (thisBoat.Distance > distances[whichRace])
+            {
+                Logger.Debug("Adding new record! Distance: {Distance} mm in the race lasting {RaceDuration}", thisBoat.Distance, times[whichRace]);
+                _records.Add(thisBoat.Distance);
+            }
+        }
     }
 }
--- 2023-AoC-CSharp/Day_06a/AoC 2023 CSharp/RawData.cs
+++ 2023-AoC-CSharp/Day_06a/AoC 2023 CSharp/RawData.cs
@@ -3,10 +3,12 @@
 public static class RawData
 {
     public static string SampleData01 => """
-                                         
+                                         Time:      7  15   30
+                                         Distance:  9  40  200
                                          """;
 
     public static string ActualData => """
-                                       
+                                       Time:        38     67     76     73
+                                       Distance:   234   1027   1157   1236
                                        """;
 }
