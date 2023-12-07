7d488784c3bc50057210514ce60933894d3d30a6
DSikes <david@sikespc.com>
2023-12-07 08:24:11 -0500

    Copied template project in preparation for day 08a

---
+++ 2023-12-AoC-CSharp/Day_08a/AoC 2023 CSharp/Utilities/StringHelpers.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+namespace AoC_2023_CSharp.Utilities;
+
+public static class StringHelpers
+{
+    public static string[] SplitAndDropFirst(this string splitString, string splitOn)
+    {
+        var firstSplit = splitString.Split(splitOn, StringSplitOptions.RemoveEmptyEntries);
+
+        var returnSplitElements = new List<string>();
+        
+        for (var i = 1; i < firstSplit.Length; i++)
+        {
+            returnSplitElements.Add(firstSplit[i]);
+        }
+
+        return returnSplitElements.ToArray();
+    }
+}
