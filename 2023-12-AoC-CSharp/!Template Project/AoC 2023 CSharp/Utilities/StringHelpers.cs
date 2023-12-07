489ba2865a328f8638a691f9efa1146a40e46df7
DSikes <david@sikespc.com>
2023-12-07 08:52:38 -0500

    renamed project template to ...project template

---
+++ 2023-12-AoC-CSharp/!Template Project/AoC 2023 CSharp/Utilities/StringHelpers.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+
+namespace AoC_2023_CSharp.Utilities;
+
+public static class StringHelpers
+{
+    public static T[] SplitAndDropFirst<T>(this string splitString, string splitOn, char? trimChar = null)
+    {
+        var firstSplit = splitString.Split(splitOn, StringSplitOptions.RemoveEmptyEntries);
+
+        var returnSplitElements = new List<T>();
+        
+        for (var i = 1; i < firstSplit.Length; i++)
+        {
+            var rawElement = firstSplit[i];
+
+            if (trimChar is not null)
+                rawElement = rawElement.Trim((char)trimChar);
+            
+            var converted = (T)Convert.ChangeType(rawElement, typeof(T));
+            
+            returnSplitElements.Add(converted);
+        }
+
+        return returnSplitElements.ToArray();
+    }
+}
