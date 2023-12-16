f4610ae4693a325b2099811817d987208b32a611
DSikes <david@sikespc.com>
2023-12-16 08:40:32 -0500

    Day 16a GUI project initial commit

---
+++ 2023-12-AoC-CSharp/Day 16a/Aoc2023CSharp/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Aoc2023CSharp.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+    
+    private readonly ILogger _logger;
+    private readonly LoggerConfiguration _loggerConfiguration;
+
+    public MainViewModel() { throw new NotImplementedException(); }     // This method being present keeps the designer happy
+    
+    public MainViewModel(LoggerConfiguration loggerConfiguration)
+    {
+        _loggerConfiguration = loggerConfiguration;
+
+        _logger = loggerConfiguration
+            .MinimumLevel.Debug()
+            .CreateLogger();
+    }
+
+}
