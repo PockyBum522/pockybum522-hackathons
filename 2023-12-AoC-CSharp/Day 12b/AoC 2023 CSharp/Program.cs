ffce0f0a001b560718fadd50ea6397ec3482ce88
DSikes <david@sikespc.com>
2023-12-18 10:04:36 -0500

    2023 Day 12a Done!

--- 2023-12-AoC-CSharp/Day 12b/AoC 2023 CSharp/Program.cs
+++ 2023-12-AoC-CSharp/Day 12b/AoC 2023 CSharp/Program.cs
@@ -14,12 +14,10 @@ internal static class Program
         ElapsedTotal.Start();
         _logger.Information("Starting!");
         
-        var rawLines = RawData.SampleData01
+        var rawLines = RawData.ActualData01
             .Split(Environment.NewLine);
 
         var answerTotal = 0;
-        
-        
         
         foreach (var line in rawLines)
         {
@@ -32,7 +30,7 @@ internal static class Program
             var linePossibilities = GetAllLinePermutations(springLine);
 
             // Parse each brute force group to see if it's correct
-            var validPermutations = CountValidPermutations(linePossibilities);
+            var validPermutations = CountValidPermutations(linePossibilities, numbers);
 
             answerTotal += validPermutations;
         }
@@ -48,7 +46,7 @@ internal static class Program
     private static string[] GetAllLinePermutations(string springLine)
     {
         var returnLines = new List<string>();
-
+        
         var unknownPositionsCount = 0;
         var finalBinaryString = "";
         
@@ -61,6 +59,8 @@ internal static class Program
                 finalBinaryString += '1';
             }
         }
+        
+        _logger.Verbose("SpringLine: {Line} | ? count: {UnknownPositionsCount}", springLine, unknownPositionsCount);
 
         var questionMarksFound = 0;
         var binaryString = "";
@@ -76,6 +76,9 @@ internal static class Program
             {
                 var character = springLine[i];
                 
+                _logger.Verbose("At char {I}: char is {Character} questionMarksFound = {QuestionMarksCount}",
+                    i, character, questionMarksFound);
+                
                 if (character == '?')
                 {
                     stringToAdd += binaryString[questionMarksFound] == '0' ? '.' : '#';
@@ -87,45 +90,83 @@ internal static class Program
 
                 stringToAdd += springLine[i];
             }
-            
+
+            questionMarksFound = 0;
             returnLines.Add(stringToAdd);
+            _logger.Verbose("Adding: {StringToAdd}", stringToAdd);
+            
+            counter++;
 
             stringToAdd = "";
         }
 
-        _logger.Debug("");
-        _logger.Debug("Strings to add: ");
-
         foreach (var line in returnLines)
         {
-            _logger.Debug(line);
+            _logger.Verbose("Line: {Line}", line);
         }
         
-        // ?###????????
-        
-        // Start with ten dots
-        
-        // .###........
-        
-        // Add that
-        
-        // First char flip to #
-        
-        // ####........
-        
-        // Add that
-        
-        // Then flip second char
-        
-        // Add
-        
-        // Now flip 3rd, do both above again
-
         return returnLines.ToArray();
     }
 
-    private static int CountValidPermutations(string[] linePossibilities)
+    private static int CountValidPermutations(string[] linePossibilities, int[] numbers)
     {
-        throw new NotImplementedException();
+        var validCount = 0;
+
+        foreach (var line in linePossibilities)
+        {
+            _logger.Debug("Comparing line: {Line} to validation numbers: {@Numbers}", line, numbers);
+            
+            if (LineValid(line, numbers))
+            {
+                _logger.Debug("Valid!");
+                
+                validCount++;
+            }
+        }
+
+        return validCount;
+    }
+
+    private static bool LineValid(string line, int[] numbers)
+    {
+        var parsedGroups = new List<int>();
+
+        var thisGroupSpringsCount = 0;
+        
+        foreach (var character in line)
+        {
+            if (character == '#')
+            {
+                thisGroupSpringsCount++;       
+            }
+            else
+            {
+                if (thisGroupSpringsCount != 0)
+                    parsedGroups.Add(thisGroupSpringsCount);
+                
+                thisGroupSpringsCount = 0;
+            }
+        }
+        
+        // Handle when line ends
+        if (thisGroupSpringsCount != 0)
+            parsedGroups.Add(thisGroupSpringsCount);
+
+        _logger.Debug("For line: {Line} test case: {TestNumbers} and parsed: {ParsedGroups}", line, numbers, parsedGroups);
+
+        if (numbers.Length != parsedGroups.Count)
+            return false;
+        
+        for (var i = 0; i < parsedGroups.Count; i++)
+        {
+            var group = parsedGroups[i];
+
+            if (parsedGroups[i] != numbers[i])
+            {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
