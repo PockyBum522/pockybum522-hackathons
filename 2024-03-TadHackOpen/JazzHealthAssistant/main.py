3ca7cbee9c97324bde3f1bdf4949f627df0ebdfa
DSikes <david@sikespc.com>
2024-03-23 13:32:14 -0400

    Update main.py

--- 2024-03-TadHackOpen/JazzHealthAssistant/main.py
+++ 2024-03-TadHackOpen/JazzHealthAssistant/main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+#!/usr/bin/env python3
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main() -> int:
+    """Entry point."""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Hello, David!")
+    
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main()
