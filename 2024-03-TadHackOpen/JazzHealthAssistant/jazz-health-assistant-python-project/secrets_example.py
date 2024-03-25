36ef8e34be138f1bdabdd1b86c63af57bb90fae4
DSikes <david@sikespc.com>
2024-03-25 08:51:38 -0400

    Create secrets_example.py

---
+++ 2024-03-TadHackOpen/JazzHealthAssistant/jazz-health-assistant-python-project/secrets_example.py
@@ -0,0 +1,19 @@
+import os
+
+print("Setting NGROK_URL environment variable")
+os.environ['NGROK_URL'] = 'https://123-456-789-000.ngrok-free.app'
+
+print("Setting PHONE_NUMBER environment variable")
+os.environ['SIGNALWIRE_FROM_NUMBER'] = '+14445556666'
+
+print("Setting NGROK_AUTH_TOKEN environment variable")
+os.environ['NGROK_AUTH_TOKEN'] = '2e6J7FU00000000000000b9k3TS_3G6R11111111111PT3nHS'
+
+print("Setting SIGNALWIRE_PROJECT_ID environment variable")
+os.environ['SIGNALWIRE_PROJECT_ID'] = '00000000-1111-2222-3333-444444444444'
+
+print("Setting SIGNALWIRE_API_TOKEN environment variable")
+os.environ['SIGNALWIRE_API_TOKEN'] = 'PT8b9c6536222222223333333334444444446186a266b9325e'
+
+print("Setting SIGNALWIRE_SPACE_URL environment variable")
+os.environ['SIGNALWIRE_SPACE_URL'] = 'my-space-name.signalwire.com'
