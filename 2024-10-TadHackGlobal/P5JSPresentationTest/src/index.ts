6f38e6c57c97cd2013e233fc19f7839a69817341
Jared M Ashcraft <jaredmashcraft@gmail.com>
2024-10-21 17:20:55 -0400

    confirmed clearing scene timer array works as expected

--- 2024-10-TadHackGlobal/P5JSPresentationTest/src/index.ts
+++ 2024-10-TadHackGlobal/P5JSPresentationTest/src/index.ts
@@ -49,10 +49,18 @@ sceneActor.subscribe((state) => {
 
 // ----***** P5JS Section *****-----
 
+// TODO: Refactor this into a class
+
 const maxScenes:number = 8;
 
 for (let i = 0; i < maxScenes; i++) {
     sceneTimers.push(0);
+}
+
+function clearAllSceneTimers() {
+    for (let i = 0; i < maxScenes; i++) {
+        clearTimeout(sceneTimers[i]);
+    }
 }
 
 new p5((p) => {
@@ -60,6 +68,16 @@ new p5((p) => {
         p.createCanvas(200, 200);
 
         //testing scene timers
+
+        sceneTimers[0] = window.setTimeout(() => { console.log(`sceneTimer 0 fires`)}, 1000);
+        sceneTimers[1] = window.setTimeout(() => { console.log(`sceneTimer 1 fires`)}, 2000);
+        sceneTimers[2] = window.setTimeout(() => { clearAllSceneTimers() }, 3000);
+        sceneTimers[3] = window.setTimeout(() => { console.log(`sceneTimer 3 fires`)}, 4000);
+        sceneTimers[4] = window.setTimeout(() => { console.log(`sceneTimer 4 fires`)}, 5000);
+        sceneTimers[5] = window.setTimeout(() => { console.log(`sceneTimer 5 fires`)}, 6000);
+        sceneTimers[6] = window.setTimeout(() => { console.log(`sceneTimer 6 fires`)}, 7000);
+
+
     };
 
     p.draw = () => {
