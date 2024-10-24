b64154eb41df05cbb871cef52d9f6f873138282e
DSikes <david@sikespc.com>
2024-10-24 14:40:57 -0400

    Add websockets server for Jared to test

---
+++ 2024-10-TadHackGlobal/WebSocketsServer/Snippets/BackgroundSocketProcessor.cs
@@ -0,0 +1,8 @@
+using System.Net.WebSockets;
+
+namespace WebSocketsServer.Snippets;
+
+internal class BackgroundSocketProcessor
+{
+    internal static void AddSocket(WebSocket webSocket, TaskCompletionSource<object> socketFinishedTcs) { }
+}
