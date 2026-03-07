88ade62a46bfff573ee7484c113e1cf9cdc48f8a
DSikes <pockybum522@gmail.com>
2026-03-06 22:22:38 -0500

    Initial commit

---
+++ 2026-03-TadHack-Apparitions/src/ApparitionsAndroidClient/ApparitionsAndroidClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ApparitionsAndroidClient.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _greeting = "Welcome to Avalonia!";
+}
---
+++ 2026-03-TadHack-Apparitions/src/ApparitionsAndroidClient/ApparitionsAndroidClient/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ApparitionsAndroidClient.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
