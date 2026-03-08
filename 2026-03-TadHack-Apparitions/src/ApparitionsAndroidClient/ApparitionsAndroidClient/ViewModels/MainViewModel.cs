915869bc5ce919dabf3bfa7bde459b8ec39b264d
DSikes <pockybum522@gmail.com>
2026-03-08 05:35:07 -0400

    Cleaning up code, tested, GPS still works

--- 2026-03-TadHack-Apparitions/src/ApparitionsAndroidClient/ApparitionsAndroidClient/ViewModels/MainViewModel.cs
+++ 2026-03-TadHack-Apparitions/src/ApparitionsAndroidClient/ApparitionsAndroidClient/ViewModels/MainViewModel.cs
@@ -1,53 +1,23 @@
 ﻿using System;
 using System.Threading.Tasks;
-using ApparitionsAndroidClient.Models;
-using ApparitionsAndroidClient.Utilities;
 using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Devices.Sensors;
 
 namespace ApparitionsAndroidClient.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    private int _count = 0;
+    private int _count;
     
     [ObservableProperty]
     private string _greeting = "Welcome to Avalonia!";
-
-    private string _gpsLocation = "No location yet";
     
     [ObservableProperty]
     private string _updateTest = "Will Update";
-
-    private string _locationRequestSuccess = "No location yet";
     
-    private Location _currentLocation = new (0, 0);
-    private Location _gpsByShop = new (28.594340, -81.381630);
-    
-    // public async Task<Location> GetCurrentLocation()
-    // {
-    //     try
-    //     {
-    //         var request = new GeolocationRequest(GeolocationAccuracy.High, TimeSpan.FromSeconds(10));
-    //         var location = await Geolocation.Default.GetLocationAsync(request);
-    //
-    //         if (location != null)
-    //         {
-    //             Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}");
-    //             return location;
-    //         }
-    //     }
-    //     catch (FeatureNotSupportedException fnsEx) { /* Handle not supported */ }
-    //     catch (PermissionException pEx) { /* Handle permission denied */ }
-    //     catch (Exception ex) { /* Unable to get location */ }
-    //
-    //     return null;
-    // }
-
-    
+    private readonly Location _gpsByShop = new (28.594340, -81.381630);
     
     [RelayCommand]
     private async Task onViewLoaded(object sender)
@@ -56,77 +26,28 @@ public partial class MainViewModel : ViewModelBase
         
         while (true)
         {
-            
             await Dispatcher.UIThread.Invoke(uiThreadWork);
         }
         
-        
-     
-        // ReSharper disable once FunctionNeverReturns
+        // ReSharper disable once FunctionNeverReturns because it's not supposed to
     }
 
     private async Task uiThreadWork()
     {
-        // Greeting = _gpsLocation;
-        await Geolocation_LocationChanged();
+        await UpdateLocation();
+        
         await Task.Delay(1000);
     }
     
-    // private async Task nonUiThreadWork()
-    // {
-    //     var rawLocation = await Geolocation.GetLocationAsync();
-    //     
-    //     if (rawLocation is null) throw new NullReferenceException("rawLocation is null");
-    //     
-    //     _currentLocation = new Location(rawLocation.Latitude, rawLocation.Longitude);
-    //     
-    //     
-    //     
-    //     _count++;
-    //     
-    //     await Task.Delay(2000);
-    // }
-    
     private async Task InitializeLocationListener()
     {
-        // Geolocation.LocationChanged += Geolocation_LocationChanged;
-        
-        // Using GeolocationAccuracy.Medium as a balance between accuracy and power consumption. Developers can adjust this value to High or Low based on their specific requirements.
         var request = new GeolocationListeningRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(1));
         var success = await Geolocation.StartListeningForegroundAsync(request);
-        var status = "Attempting to start listening";
 
-        UpdateTest = success.ToString();
-
-//          if (success)
-//          {
-//              _gpsLocation = $"""
-//                              Shop Lat: {_gpsByShop.Latitude}, 
-//                              Shop Long: {_gpsByShop.Longitude}
-//
-//                              Lat: {_currentLocation.Latitude}, 
-//                              Long: {_currentLocation.Longitude}
-//
-//                              Calculated distance:
-//                              {Location.CalculateDistance(_currentLocation, _gpsByShop, DistanceUnits.Kilometers)} km
-//
-//                              Count: 
-//                              {_count}
-//
-//                              Success on listening init:
-//                              {_locationRequestSuccess}
-//                              """;
-//              status =  "Success";
-//          }
-//          else
-//          {
-//              status = "Couldn't start listening.";
-//          }
-//          
-         // _locationRequestSuccess = status;
+        UpdateTest = $"Location listening start success: {success.ToString()}";
     }
 
-    async Task Geolocation_LocationChanged()
+    async Task UpdateLocation()
     {
         var currentLocation = await Geolocation.GetLastKnownLocationAsync();
 
@@ -147,9 +68,6 @@ public partial class MainViewModel : ViewModelBase
 
                         Count: 
                         {_count++}
-
-                        Success on listening init:
-                        {_locationRequestSuccess}
                         """;
     }
 }
