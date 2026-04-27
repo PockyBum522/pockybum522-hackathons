c627539f2ce3bc580bd3ff4d42e48d26016e0ff4
Jared M Ashcraft <jaredmashcraft@gmail.com>
2026-04-26 21:37:17 -0400

    Added necessary file for ApparitionsAndroidClient.Desktop/Program.cs to configure

---
+++ 2026-03-TadHack-Apparitions/src/ApparitionsAndroidClient/ApparitionsAndroidClient.Desktop/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using ApparitionsAndroidClient;
+using Avalonia;
+
+namespace ApparitionsAndroidClient.Desktop;
+
+internal sealed class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+    }
+}
